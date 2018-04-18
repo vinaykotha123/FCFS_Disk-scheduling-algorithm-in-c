8fa3b08eab778790ddd3fcbf26e5c526bc5cbe59
vinaykotha123 <38245498+vinaykotha123@users.noreply.github.com>
2018-04-18 19:33:54 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -41,55 +41,5 @@ void password()
 		printf("\nlogin unsucessfull \n please enter the valid username and password");
 	}
 }
-void program()
-    {int choice,m,n,x,v,start,i,j,pos,min,a[15],count;
-     count=0;
-     system("cls");
-     printf("\nEnter the number of cylinders :");
-     scanf("%d",&m);
-     printf("\nEnter the number of requests :");
-     scanf("%d",&n);
-     printf("\nEnter previous requesst position:");
-     scanf("%d",&v);
-     printf("\nEnter current position :");
-     scanf("%d",&start);
-     printf("\nEnter the request queue :");
-     for(i=0;i<n;i++)
-                     {scanf("%d",&a[i]);
-                      if(a[i]>=m)
-                                 {printf("\ninvalid input");
-                                  scanf("%d",&a[i]);
-                                  }
-                      }
-       system("cls");
-       printf("Data is recorded.............\n\nKindly select the option");
-       printf("\n\nDISK SCHEDULING ALGORITHMS\n1. FCFS\n OR \nPRESS ANY KEY TO EXIT");
-        printf("\nEnter choice :");
-        scanf("%d",&choice);
-        count=0;
-        x=start;
-        switch(choice)
-                      {case 1:    system("cls");
-					      printf("\t\t\t\t\t********WELCOME TO DISK SCHEDULING ALGORITHM OF FCFS**********\n\n");
-                                           
-                              printf("Scheduling services the request in the order that follows:\n%d\t",start);
-                              for(i=0;i<n;i++)
-                                              {
-											  x-=a[i];
-                                               if(x<0)
-                                                      x=-x;
-                                               count+=x;
-                                               x=a[i];
-                                               printf("%d\t",x);
-                                               }
-                              printf("\nTotal distance (in cylinders) is :%d ",count);
-                              break;
-                              	default:
-				printf("EXIT\n");
-				break;
-                       }
-       
-        
-    }
                                      
 
